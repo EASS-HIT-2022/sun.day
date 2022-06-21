196c9f6f7aae8f62f5efaf92d553491096dcb762
Niv Huga <niv656@gmail.com>
2022-06-21 23:36:12 +0300

    🎏 [New Feature]: Add support to env variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_URL: process.env.API_URL,
+  },
 };
 
 module.exports = nextConfig;
